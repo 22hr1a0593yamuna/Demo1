fea16f86ca9ebc0c36225540a3477149a190e233
22hr1a0593yamuna <154886226+22hr1a0593yamuna@users.noreply.github.com>
2024-01-12 15:42:26 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,7 @@
+package mypack;
+public class A{
+public void msg(){
+System.out.println("hello from class A");}
+public void msgA(){
+System.out.println("last program for class A in msgA");}
+}
---
+++ A.java.java
@@ -0,0 +1,6 @@
+package mypack;
+public class A{
+public void msg(){
+System.out.println("hello from class A");}
+System.out.println("last program for class A in msgA");}
+}
